7f16875d11291fa7adc7c8c98e9c42293108bdab
Martí Climent <marticlilop@gmail.com>
2023-04-13 16:05:06 +0200

    Update logo.py
    improvements

--- logo.py
+++ logo.py
@@ -19,17 +19,22 @@ color = (0, 0, 0)
 logostr += f"setpensize 2000 bk 1000 fd 2000 bk 1000 setpensize 1 pu fd {im.height//2} rt 90 bk {im.width//2} pd\n"
 fdcount = 0
 for y in range(im.height):
+    fdAmount = 0
     for x in range(im.width):
         color = px[x, y][0:3]
         if(len(color)==4) and color==(0, 0, 0):
             color=(255,255,255)
         if color != oldcolor:
             oldcolor = color
-            logostr += f"color [{color[0]} {color[1]} {color[2]}] "
-        logostr += "fd 1\n"
+            logostr += f"fd {fdAmount} color [{color[0]} {color[1]} {color[2]}]\n"
+            fdAmount = 0
+        fdAmount += 1
         fdcount += 1
+    if fdAmount != 0:
+        logostr += f"fd {fdAmount} "
     logostr += f"pu bk {fdcount} rt 90 fd 1 lt 90 pd\n"
     fdcount = 0
+    print(str(int(y/im.height*100))+"%")
 
 root = tk.Tk()
 
